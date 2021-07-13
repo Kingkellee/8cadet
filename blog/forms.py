4cd303f70211c28baabf8b656d0a298a29eea553
Kelly Iyogun <55467245+Kingkellee@users.noreply.github.com>
2021-07-13 01:07:01 +0100

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import Post, Category
 from ckeditor.fields import CKEditorWidget
 
-#choices = Category.objects.all().values_list('name', 'name')
+choices = Category.objects.all().values_list('name', 'name')
 choice_list = []
-#for item in choices:
-#    choice_list.append(item)
+for item in choices:
+    choice_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
